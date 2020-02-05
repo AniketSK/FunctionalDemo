8f6d2b00354c3644d6d4c6e4abba0980825bd4ee
Aniket Kadam <anikadamg@gmail.com>
2020-02-05 19:28:50 +0530

    Modified FailableOperations to not require a custom interface

--- app/src/main/java/com/aniketkadam/functionaldemo/FailableOperations.kt
+++ app/src/main/java/com/aniketkadam/functionaldemo/FailableOperations.kt
@@ -1,10 +1,19 @@
 package com.aniketkadam.functionaldemo
 
-sealed class EitherDataOrError<A : Exception, B> {
+/**
+ * A type that can encapsulate the result, no matter what it is
+ */
+sealed class EitherDataOrError<A : Exception , B> {
     data class Error<A : Exception, B>(val left: A) : EitherDataOrError<A, B>()
     data class Data<A : Exception, B>(val right: B) : EitherDataOrError<A, B>()
 }
 
+/**
+ * Abstract the process of
+ * 1. Running a failable operation
+ * 2. If it succeeds, run the success case
+ * 3. If it fails, run the failure case
+ */
 fun <A : Exception, B> executeFailableOperation(
     operation: () -> EitherDataOrError<A, B>,
     onSuccess: (B) -> Unit,
@@ -14,3 +23,15 @@ fun <A : Exception, B> executeFailableOperation(
         is EitherDataOrError.Error -> onError(result.left)
         is EitherDataOrError.Data -> onSuccess(result.right)
     }
+
+
+inline fun <reified A : Exception, B> convertToEither( operation: () -> B  ) : EitherDataOrError<A,B> {
+    return try{
+        EitherDataOrError.Data(operation())
+    } catch (error : Exception) {
+        if(error is A)
+            EitherDataOrError.Error(error)
+        else
+            throw error
+    }
+}
--- app/src/main/java/com/aniketkadam/functionaldemo/SimulatingNetworkHandlingErrors.kt
+++ app/src/main/java/com/aniketkadam/functionaldemo/SimulatingNetworkHandlingErrors.kt
@@ -6,36 +6,35 @@ class SimulatingNetworkHandlingErrors {
 
     fun storeDataFromNetwork(
         requestParam: String,
-        someApi: SomeApiMaybeErrors,
+        someApi: SomeApi,
         validation: Validation,
         secondApi: SecondApi,
         database: Database,
         view: View
     ) {
         executeFailableOperation(
-            operation = { someApi.getItemsFromNetwork(requestParam) },
+            operation = { convertToEither<NetworkErrorException, List<NetworkItem>> {someApi.getItemsFromNetwork(requestParam)} },
             onSuccess = { data -> onSuccess(data, validation, secondApi, database) },
             onError = { error -> onError(error, view) }
         )
     }
 
-    private fun onError(error : NetworkErrorException,
-                        view : View) {
+    private fun onError(
+        error: NetworkErrorException,
+        view: View
+    ) {
         view.showError("Error was ${error.message}")
     }
 
-    private fun onSuccess(data: List<NetworkItem>,
-                          validation: Validation,
-                          secondApi: SecondApi,
-                          database: Database) {
+    private fun onSuccess(
+        data: List<NetworkItem>,
+        validation: Validation,
+        secondApi: SecondApi,
+        database: Database
+    ) {
         data.filter(validation::isValid)
             .map(secondApi::getSecondaryInfo)
             .map(database::insert)
     }
 
 }
-
-interface SomeApiMaybeErrors {
-    fun getItemsFromNetwork(param: String): EitherDataOrError<NetworkErrorException, List<NetworkItem>>
-}
-
