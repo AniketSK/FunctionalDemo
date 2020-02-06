5db88640f1486064672e8f20219836a15f9a1e10
Aniket Kadam <anikadamg@gmail.com>
2020-02-06 17:54:58 +0530

    Added an extension function version of the foreach

--- app/src/main/java/com/aniketkadam/functionaldemo/ForEachImplementation.kt
+++ app/src/main/java/com/aniketkadam/functionaldemo/ForEachImplementation.kt
@@ -7,3 +7,12 @@ fun <T, R> customForEach(list: List<T>, operation: (T) -> R): List<R> {
     }
     return resultList
 }
+
+// Extension function version
+fun <T, R> List<T>.extensionCustomForEach(operation: (T) -> R): List<R> {
+    val resultList: MutableList<R> = mutableListOf()
+    for (item in this) {
+        resultList.add(operation(item))
+    }
+    return resultList
+}
--- app/src/test/java/com/aniketkadam/functionaldemo/ForEachImplementationKtTest.kt
+++ app/src/test/java/com/aniketkadam/functionaldemo/ForEachImplementationKtTest.kt
@@ -14,4 +14,13 @@ class ForEachImplementationKtTest{
 
         assertThat(stringVersion, equalTo(listOf("1","2","3","4","5")))
     }
+
+    @Test
+    fun `custom extension function for each works as expected`() {
+        val intList = listOf(1,2,3,4,5)
+
+        val stringVersion : List<String> = intList.extensionCustomForEach { number -> number.toString() }
+
+        assertThat(stringVersion, equalTo(listOf("1","2","3","4","5")))
+    }
 }
