8242dadbf868ffbbb0628726c9ab6ebec3cc4395
Aniket Kadam <anikadamg@gmail.com>
2020-02-06 20:20:02 +0530

    Renamed tests, added new ones

--- app/src/test/java/com/aniketkadam/functionaldemo/FunctionalOperatorsImplementationKtTest.kt
+++ app/src/test/java/com/aniketkadam/functionaldemo/FunctionalOperatorsImplementationKtTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 class FunctionalOperatorsImplementationKtTest {
 
     @Test
-    fun `custom for each works as expected`() {
+    fun `custom map works as expected`() {
         val intList = listOf(1, 2, 3, 4, 5)
 
         fun numberToString(number: Int): String {
@@ -20,12 +20,26 @@ class FunctionalOperatorsImplementationKtTest {
     }
 
     @Test
-    private fun `custom extension function for each works as expected`() {
+    fun `custom extension function map works as expected`() {
         val intList = listOf(1, 2, 3, 4, 5)
 
         val stringVersion: List<String> =
             intList.extensionCustomMap { number -> number.toString() }
 
         assertThat(stringVersion, equalTo(listOf("1", "2", "3", "4", "5")))
+    }
+
+    @Test
+    fun `custom extension function for each works as expected`() {
+        val intList = listOf(1,2)
+
+        customForEach(intList, { number -> println(number) })
+    }
+
+    @Test
+    fun `custom for each works as expected`() {
+        val intList = listOf(1,2)
+
+        intList.extensionCustomForEach { number -> println(number) }
     }
 }
