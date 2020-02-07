caff2f98e39aeb7b6c0b29755e14dc25bcd7bc99
Aniket Kadam <anikadamg@gmail.com>
2020-02-07 13:19:13 +0530

    Added some nicer generic param names

--- app/src/main/java/com/aniketkadam/functionaldemo/FunctionalOperatorsImplementation.kt
+++ app/src/main/java/com/aniketkadam/functionaldemo/FunctionalOperatorsImplementation.kt
@@ -1,7 +1,7 @@
 package com.aniketkadam.functionaldemo
 
-fun <T, R> customMap(list: List<T>, operation: (T) -> R): List<R> {
-    val resultList: MutableList<R> = mutableListOf()
+fun <I, O> customMap(list: List<I>, operation: (I) -> O): List<O> {
+    val resultList: MutableList<O> = mutableListOf()
     for (item in list) {
         resultList.add(operation(item))
     }
@@ -9,8 +9,8 @@ fun <T, R> customMap(list: List<T>, operation: (T) -> R): List<R> {
 }
 
 // Extension function version
-fun <T, R> List<T>.extensionCustomMap(operation: (T) -> R): List<R> {
-    val resultList: MutableList<R> = mutableListOf()
+fun <I, O> List<I>.extensionCustomMap(operation: (I) -> O): List<O> {
+    val resultList: MutableList<O> = mutableListOf()
     for (item in this) {
         resultList.add(operation(item))
     }
@@ -18,13 +18,13 @@ fun <T, R> List<T>.extensionCustomMap(operation: (T) -> R): List<R> {
 }
 
 
-fun <T> customForEach(list : List<T>, operation: (T) -> Unit) {
-    for (item : T in list) {
+fun <I> customForEach(list : List<I>, operation: (I) -> Unit) {
+    for (item  in list) {
         operation(item)
     }
 }
 
-fun <T> List<T>.extensionCustomForEach(operation: (T) -> Unit) {
+fun <I> List<I>.extensionCustomForEach(operation: (I) -> Unit) {
     for (item in this) {
         operation(item)
     }
